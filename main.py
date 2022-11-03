50104fe83237bddcb83c21d6692f3559e0da3b93
Ankitakanse <114207806+Ankitakanse@users.noreply.github.com>
2022-11-04 01:54:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+#from pandas_profiling import ProfileReport
+#from streamlit_pandas_profiling import st_profile_report
 
 # Web App Title
 st.markdown('''
